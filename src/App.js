987b22c4a2ed31b72dd7318aac5eb8ec9fc689d1
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-07-15 14:53:34 +0530

    created state variable with useState hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const messages = [
   "Learn React ⚛️",
@@ -7,33 +7,39 @@ const messages = [
 ];
 
 const App = () => {
-  const step = 1;
+  const [step, setStep] = useState(1);
 
   function handlePrevious(){
-    alert("Previous");
+    if(step > 1) setStep(step-1);
   }
 
   function handleNext(){
-    alert("Next");
+    if(step < 3) setStep(step+1); 
   }
 
   return (
     <div className='steps'>
       <div className='numbers'>
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className='message'>Step {step}: {messages[step-1]}</p>
 
       <div className='buttons'>
-        <button style={{backgroundColor: '#7950f2', color: '#fff'}} 
-        onClick={handlePrevious}
-        onMouseEnter={() => alert("TEST")}
-        >Previous</button>
-        <button style={{backgroundColor: '#7950f2', color: '#fff'}} 
-        onClick={handleNext}>Next</button>
+        <button 
+          style={{backgroundColor: '#7950f2', color: '#fff'}} 
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button 
+          style={{backgroundColor: '#7950f2', color: '#fff'}} 
+          onClick={handleNext}
+        >
+          Next
+        </button>
       </div>
     </div>
   )
