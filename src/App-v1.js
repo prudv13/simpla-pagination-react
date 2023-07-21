6f9a3b54c3703c88c48e1ae6e38fb0d65f81bb40
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-07-21 16:21:39 +0530

    reusability with children prop

--- src/App-v1.js
+++ src/App-v1.js
@@ -6,7 +6,7 @@ const messages = [
   "Invest your new income 🤑",
 ];
 
-export default function App() {
+const App = () => {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
@@ -29,41 +29,28 @@ export default function App() {
           <div className={step >= 3 ? "active" : ""}>3</div>
         </div>
 
-        <StepMessage className='message' step={step}>{messages[step-1]}</StepMessage>
+        <p className='message'>Step {step}: {messages[step-1]}</p>
 
         <div className='buttons'>
-          <Button
-            textColor='#fff'
-            bgColor='#7950f2'
+          <button 
+            style={{backgroundColor: '#7950f2', color: '#fff'}} 
             onClick={handlePrevious}
-          ><span>👈</span> Previous</Button>
-          <Button
-            textColor='#fff'
-            bgColor='#7950f2'
+          >
+            Previous
+          </button>
+          <button 
+            style={{backgroundColor: '#7950f2', color: '#fff'}} 
             onClick={handleNext}
-          >Next <span>👉</span></Button>
+          >
+            Next
+          </button>
         </div>
       </div> )
     }
     </Fragment>
+    
   )
 }
 
-function StepMessage({step, children}){
-  return (
-    <div className='message'>
-      <h3>Step {step}:</h3> {children}
-    </div>
-  );
-}
+export default App;
 
-function Button({textColor, bgColor, onClick, children}){
-  return (
-    <button
-      style={{backgroundColor: bgColor, color: textColor}} 
-      onClick={onClick}
-    >
-    {children}
-    </button>
-  );
-}
